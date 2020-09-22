911048fdbb648fdd9f538c7afecf5d66f337f614
Noah Guillory <noah@noahguillory.com>
2020-09-22 18:45:54 -0500

    Initial Commit

---
+++ MakeItCount/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MakeItCount
+//
+//  Created by Noah Guillory on 9/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MakeItCount/MakeItCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeItCountApp.swift
+//  MakeItCount
+//
+//  Created by Noah Guillory on 9/22/20.
+//
+
+import SwiftUI
+
+@main
+struct MakeItCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
